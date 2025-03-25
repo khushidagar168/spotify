7b66f612e500a9704ce049be196bc5b9696c37c1
khushi <khushidagariitian@gmai.com>
2025-03-26 00:21:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+async function getSongs (){
+    
+    let a= await fetch("http://127.0.0.1:3000/songs/")
+   
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs =[]
+    
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1])
+        }
+        
+    }
+    console.log(songs)
+    return songs
+}
+
+async function main(){
+    let songs = await getSongs()
+    
+
+     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+     
+     for (const song of songs ) {
+       
+        songUL.innerHTML= songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>` ;
+        
+     }
+
+
+
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("ontimeupdate", () => {
+        console.log(audio.duration , audio.currentSrc ,audio.currentTime)
+    })
+
+    
+}
+main()
